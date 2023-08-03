d82e77d1434e08a2d3c562099c4d43934ed762db
Ekaterina Kiryanova <109188496+KrKate@users.noreply.github.com>
2023-08-04 00:51:22 +0500

    fix: fix eslintrc.cjs

--- ecommerce-application/.eslintrc.cjs
+++ ecommerce-application/.eslintrc.cjs
@@ -7,9 +7,30 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true
+  },
+  plugins: ['prettier', 'import', '@typescript-eslint'],
+  rules: {
+    'no-debugger': 'off',
+    'no-console': 0,
+    'class-methods-use-this': 'off',
+    '@typescript-eslint/no-explicit-any': 2,
+    '@typescript-eslint/dot-notation': ['error', { allowKeywords: true }],
+    'eol-last': ['error', 'always']
+  },
+  parser: '@typescript-eslint/parser'
 }
