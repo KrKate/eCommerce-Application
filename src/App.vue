95ec305d248ef52254a4854bc0a8b176555ddf20
KirillDemyanenko <demyanenkokg@gmail.com>
2023-08-14 13:02:18 +0300

    fix: lint fixes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import AppHeader from '@/components/AppHeader.vue'
-import { useUserStore } from '@/stores/authorization'
-import { onMounted } from 'vue'
-const store = useUserStore()
-onMounted(() => {
-  store.fetchToken()
-})
 </script>
 
 <template>
--- src/stores/types.ts
+++ src/stores/types.ts
@@ -6,46 +6,46 @@
 // }
 
 export type CategoryResponse = {
-    limit: number
-    offset: number
-    count: number
-    total: number
-    results: Category[]
+  limit: number
+  offset: number
+  count: number
+  total: number
+  results: Category[]
 }
 
 type Category = {
-    id: string
-    version: number
-    versionModifiedAt: string
-    lastMessageSequenceNumber: number
-    createdAt: string
-    lastModifiedAt: string
-    lastModifiedBy: {
-        isPlatformClient: boolean
-        user: {
-            typeId: string
-            id: string
-        }
+  id: string
+  version: number
+  versionModifiedAt: string
+  lastMessageSequenceNumber: number
+  createdAt: string
+  lastModifiedAt: string
+  lastModifiedBy: {
+    isPlatformClient: boolean
+    user: {
+      typeId: string
+      id: string
     }
-    createdBy: {
-        isPlatformClient: boolean
-        user: {
-            typeId: string
-            id: string
-        }
+  }
+  createdBy: {
+    isPlatformClient: boolean
+    user: {
+      typeId: string
+      id: string
     }
-    key: string
-    name: {
-        'en-US': string
-    }
-    slug: {
-        'en-US': string
-    }
-    description: {
-        'en-US': string
-    }
-    ancestors: []
-    orderHint: string
-    externalId: string
-    assets: []
+  }
+  key: string
+  name: {
+    'en-US': string
+  }
+  slug: {
+    'en-US': string
+  }
+  description: {
+    'en-US': string
+  }
+  ancestors: []
+  orderHint: string
+  externalId: string
+  assets: []
 }
