11779ae0c5b539f88b52dd116a85d76674080e56
KirillDemyanenko <demyanenkokg@gmail.com>
2023-08-24 20:55:43 +0300

    feat: add link to profile in header

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <p>Profile page</p>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "ProfileView"
+}
+</script>
+
+<style scoped>
+main {
+  display: flex;
+  margin: auto;
+  padding-top: 200px;
+  font-size: 2rem;
+  font-weight: 700;
+  font-style: italic;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
