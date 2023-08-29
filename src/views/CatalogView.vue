a91a038856701166474d3b150e3da5586108edcf
Ekaterina Kiryanova <109188496+KrKate@users.noreply.github.com>
2023-08-29 13:57:51 +0500

    feat: add first card in Catalog

--- src/views/CatalogView.vue
+++ src/views/CatalogView.vue
@@ -1,6 +1,13 @@
 <template>
   <main>
-    <p>Catalog page</p>
+    <div class="cards-container">
+    <div class="product-card">
+    <h3 class="product-title">Mewtwo</h3>
+    <img src="../../src/assets/images/mewtwo.png" alt="Product Image" class="product-image">
+    <div class="product-description">Its DNA is almost the same as Mew’s. However, its size and disposition are vastly different.</div>
+    <div class="product-price">500$</div>
+  </div>
+  </div>
   </main>
 </template>
 
@@ -13,6 +20,44 @@ main {
   font-weight: 700;
   font-style: italic;
   flex-direction: column;
-  align-items: center;
+}
+
+.cards-container {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 20px;
+}
+.product-card {
+  max-height: 500px;
+  max-width: 200px;
+  display: flex;
+  flex-direction: column;
+  border: 2px solid #cccccc;
+  padding: 10px;
+}
+
+.product-image {
+  object-fit: cover;
+  margin-bottom: 10px;
+}
+
+.product-title {
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 5px;
+  background-color: black;
+  color: white;
+  text-align: center;
+}
+
+.product-description {
+  margin-bottom: 5px;
+  font-size: 1rem;
+  text-align: justify;
+}
+
+.product-price {
+  font-size: 1.2rem;
+  text-align: center;
 }
 </style>
