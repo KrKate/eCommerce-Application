82e1c0d1785f37bf748cf6ab431a2fd08b7a0004
Ekaterina Kiryanova <109188496+KrKate@users.noreply.github.com>
2023-09-04 12:06:42 +0500

    feat: add card info

--- src/views/ProductInfoView.vue
+++ src/views/ProductInfoView.vue
@@ -1,6 +1,24 @@
+<template>
+  <main>
+    <div class="images-bar"></div>
+    <div class="alternate-images"></div>
+    <div class="main-image">
+      <img :src=" product.masterData.current.masterVariant.images[0].url" alt="Pokemon">
+    </div>
+    <div class="main-description">
+      <h1 class="title">{{ product.masterData.current.name['en-US'] }}</h1>
+      <div class="description">{{ product.masterData.current.description['en-US'] }}</div>
+      <add-button>Добавить в корзину</add-button>
+    </div>
+    <!-- <p>{{ product }}</p> -->
+  </main>
+</template>
+
+
+
 <script lang="ts">
 import { useUserStore } from '@/stores/authorization'
-import { Product } from '@/stores/types'
+import { type Product } from '@/stores/types'
 
 export default {
   name: 'ProductInfoView',
@@ -18,11 +36,7 @@ export default {
 }
 </script>
 
-<template>
-  <main>
-    <p>{{ product }}</p>
-  </main>
-</template>
+
 
 <style scoped lang="scss">
 main {
@@ -32,7 +46,12 @@ main {
   font-size: 2rem;
   font-weight: 700;
   font-style: italic;
-  flex-direction: column;
   align-items: center;
 }
+
+.main-description {
+  display: flex;
+  flex-direction: column;
+}
+
 </style>
